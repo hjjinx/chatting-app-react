9549cc5a5e3285cb0f17e7449fefcc9afab00fe2
Himanshu Jain <hjmanojain.8@gmail.com>
2020-12-12 19:47:35 +0530

    established connection to socket.io in backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { io } from "socket.io-client";
+import { useEffect } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const socket = io("localhost:8080", { port: 8080 });
+    socket.on("connect", () => {
+      console.log(socket);
+      socket.on("socket", console.log);
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          style={{ marginRight: 2 }}
+          color="inherit"
+          aria-label="menu"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          Chat
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
   );
 }
 
